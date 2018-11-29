b764e6380fa7fcf1547f51de7fb391384d4c4f82
zxc <475977851@qq.com>
2018-11-30 00:16:34 +0800

    睡觉前提交

---
+++ src/com/zc/exampledoc/DocTest.java
@@ -0,0 +1,52 @@
+package com.zc.exampledoc;
+
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * example doc项目中的测试代码
+ * 
+ * @author zcAsus
+ *
+ */
+public class DocTest {
+
+	@Test
+	public void test() {
+		DocumentManager docManager = new DocumentManager();
+		IDocument doc = new WordDocument();
+		docManager.setDocument(doc);
+		docManager.read();
+		docManager.write();
+	}
+	
+	@Test
+	public void test2() {
+		IDocument doc=new PdfDocument();
+		doc.read();
+		doc.write();
+	}
+	
+
+	@SuppressWarnings("resource")
+	@Test
+	public void testSpring() {
+		// 下述xml的地址应该是斜杠还是点？？
+		ApplicationContext context = new ClassPathXmlApplicationContext("com/zc/exampledoc/contextDoc.xml");
+		IDocument doc = (IDocument) context.getBean("excelDoc");
+		doc.read();
+		doc.write();
+	}
+	
+	@SuppressWarnings("resource")
+	@Test
+	public void testSpring2() {
+		ApplicationContext context=new ClassPathXmlApplicationContext("com/zc/exampledoc/contextDoc.xml");
+		DocumentManager docManager=(DocumentManager) context.getBean("docManager");
+		docManager.write();
+		docManager.read();
+		
+	}
+
+}
--- src/com/zc/exampledoc/DocumentManager.java
+++ src/com/zc/exampledoc/DocumentManager.java
@@ -1,5 +1,26 @@
 package com.zc.exampledoc;
 
 public class DocumentManager {
+	private IDocument doc;
+	
+	public DocumentManager() {
+		
+	}
+
+	public DocumentManager(IDocument doc) {
+		this.doc = doc;
+	}
+
+	public void setDocument(IDocument doc) {
+		this.doc = doc;
+	}
+
+	public void read() {
+		this.doc.read();
+	}
+
+	public void write() {
+		this.doc.write();
+	}
 
 }
